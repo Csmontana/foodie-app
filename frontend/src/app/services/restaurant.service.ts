3b3b4576b7cf6176c4947cbf9d3cc5ed05b2db79
Cristina Montana <cristina.s.montana@gmail.com>
2025-04-15 23:41:22 -0700

    Initial commit of foodie-app with Docker, Angular, and Spring Boot setup

--- frontend/src/app/services/restaurant.service.ts
+++ frontend/src/app/services/restaurant.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Restaurant } from '../models/restaurant.model';
+import { environment } from 'environments/environments';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
-  private apiUrl = 'http://localhost:8080/api/restaurants';
+  private apiUrl = `${environment.apiUrl}/restaurants`;
 
-  constructor(private http: HttpClient) {}
+   constructor(private http: HttpClient) {}
 
   getRestaurants(): Observable<Restaurant[]> {
     return this.http.get<Restaurant[]>(this.apiUrl);
